ce2fb3964b545f423c0f366aa0ed000816800450
ejwork7 <u4683819479@gmail.com>
2026-01-22 11:50:41 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+  backend "s3" {
+    bucket         = "jenkins-state-ej2"   
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+resource "aws_s3_bucket" "example" {
+bucket = "jenkins-test-bucket-ej"
+}
